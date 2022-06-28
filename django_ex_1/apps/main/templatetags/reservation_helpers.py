51bffdcbe59dcfab7d4ea1232fbe97fb56b60bde
ToxaSmet <antony.smeet@gmail.com>
2022-06-28 20:43:12 +0300

    better template filter

--- django_ex_1/apps/main/templatetags/reservation_helpers.py
+++ django_ex_1/apps/main/templatetags/reservation_helpers.py
@@ -4,7 +4,9 @@ register = template.Library()
 
 
 @register.filter
-def get_previous_reservation_id(reservations, index):
+def get_previous_reservation_id(page, index):
+    reservations = page.paginator.object_list_with_previous
+
     # NOTE: when using PreviousObjectPaginator we must increase index,
     # as paginator has one more object in its object_list_with_previous property,
     # in order to match indexes with page objects
